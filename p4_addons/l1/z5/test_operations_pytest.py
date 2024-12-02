1f8b2dc83d77494d506d35f0cff4cc8f88eed42b
Max <max.ivashura@gmail.com>
2024-12-02 18:48:49 +0300

    Fix names

--- p4_addons/l1/z5/test_operations_pytest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from operations import divide
-
-def test_divide_by_zero():
-    """Проверяет, что деление на ноль вызывает исключение ZeroDivisionError."""
-    with pytest.raises(ZeroDivisionError):
-        divide(10, 0)
